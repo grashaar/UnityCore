c5cc0443e327ab2136cfcf5c713d01508592057d
Grashaar <grashaar@outlook.com>
2019-12-04 22:54:35 +0700

    Add ValidateIndex methods

--- Global/StringExtensions.cs
+++ Global/StringExtensions.cs
@@ -8,4 +8,7 @@
     /// <returns></returns>
     public static string Or(this string self, string other)
         => string.IsNullOrEmpty(self) ? other : self;
+
+    public static bool ValidateIndex(this string self, int index)
+        => self != null && index >= 0 && index < self.Length;
 }
